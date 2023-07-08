420ee7711bdb50574f04ac78127d15f383bb6b03
Mr. Ormus <ormusorsen@gmail.com>
2023-07-08 21:37:35 +0500

    movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,6 +2,14 @@ export default function Movie (props) {
   const { Title, Year, imdbID, Type, Poster } = props;
 
   return (
-    <h1>Hello World</h1>
+    <div key={imdbID} className="card movie">
+      <div className="card-image waves-effect waves-block waves-light">
+        <img src={Poster} alt={Title} className="activator" />
+      </div>
+      <div className="card-content">
+        <span className="card-title activator grey-text text-darken-4">{Title} <i className="material-icons right">more_vert</i></span>
+        <p>{Year} <span className="right">{Type}</span></p>
+      </div>
+    </div>
   )
 }
