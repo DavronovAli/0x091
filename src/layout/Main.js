5742518f5dedb273f4e4c2d92e0d66752e8d27d5
Mr. Ormus <ormusorsen@gmail.com>
2023-07-09 12:16:08 +0500

    mybox

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -10,21 +10,22 @@ export default class Main extends React.Component {
   componentDidMount() {
     fetch("https://www.omdbapi.com/?apikey=fedb0abd&s=panda")
       .then(response => response.json())
-      .then(data => this.setState({movies: data.Search}))
+      .then(data => this.setState({movies: data.Search , loading: false}))
   }
 
 
   searchMovies = (str = 'panda', type = 'all') => {
+    this.setState({loading: true})
     fetch(`https://www.omdbapi.com/?apikey=fedb0abd&s=${str}${type !== 'all' ? `&type=${type}` : '' }`)
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search }));
+      .then((data) => this.setState({ movies: data.Search , loading: false}));
   }
 
   render() {
     return (
       <div className="container content">
         <Search searchMovies={this.searchMovies} />
-        {this.state.movies.length ? (<Movies movies={this.state.movies} />) : <Loader />}
+        {this.state.loading ? <Loader /> : (<Movies movies={this.state.movies} />)}
       </div>
     )
   }
